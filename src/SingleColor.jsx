ade1f60447a2f8db82420368422c965267504cc9
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-05-17 01:08:46 -0500

    styling the single component color

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
+const SingleColor = ({rgb,weight,index,hexColor}) => {
+  const [alert, setalert] = useState(false);
 
+  const bcg=rgb.join(',');
+  const hex=rgbToHex(...rgb);
+
+  console.log(hex)
   rgbToHex
   return (
-    <div>SingleColor</div>
+    <article className={`color ${index >10 && 'color-light'}`} style={{ background:`rgb(${bcg})`}}>
+      <p className="percentage-value">{weight}</p>
+      <p className="color-value">{hexColor}</p>
+    </article>
   )
 }
 
